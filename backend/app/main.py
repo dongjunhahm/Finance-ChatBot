af7d9cc11cc9e9a5c8fee5183fdf19186958d97b
Dongjun Hahm <dhahm@ucsc.edu>
2025-04-25 21:51:58 -0700

    redoing imports > forgot to go out to root directory for last commit

---
+++ backend/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI, Request
+from fastapi.middleware.cors import CORSMiddleware
+from backend.app.rag import generate_answer
+
+app = FastAPI()
+
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:3000"],
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+@app.post("/api/ask")
+async def ask_question(request: Request):
+  return("hello wordl")
+
+
