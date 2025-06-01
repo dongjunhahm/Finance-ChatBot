7e2c7a128fb9b9b89e82a1dc77d597e06cdccfd9
Dongjun Hahm <dhahm@ucsc.edu>
2025-05-31 21:43:55 -0700

    was running into asyncio issues again. fixed by running retrieval and llm startup on intiation, using the lifespan and asynccontextmanager functions. currently, successfully accesses links and enters articles, fails to access specific text. almost working

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,30 @@
+import sys
+import asyncio
+
+if sys.platform == "win32":
+    loop = asyncio.ProactorEventLoop()
+    asyncio.set_event_loop(loop)
+
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from app.temprag import generate_answer
+from app import temprag
+from contextlib import asynccontextmanager
 
-app = FastAPI()
+
+retriever = None
+docs = []
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+  global retriever, docs
+
+  docs = await temprag.main()
+  retriever = temprag.initialize_retriever(docs)
+
+  yield
+
+app = FastAPI(lifespan=lifespan)
 
 app.add_middleware(
   CORSMiddleware,
@@ -16,7 +38,7 @@ app.add_middleware(
 async def ask_question(request: Request):
   data = await request.json()
   question = data["question"]
-  answer = generate_answer(question)
+  answer = generate_answer(question, retriever)
   return {"answer": answer}
 
 
